ec9112bb791b9534cf949a284520d5cfa042fe4f
Keddad Youcef <youcef.keddad@ens.uvsq.fr>
2023-01-05 21:39:01 +0100

    Add Iterative Methods declarations

--- include/lib_poisson1D.h
+++ include/lib_poisson1D.h
@@ -22,3 +22,13 @@ void write_xy(double* vec, double* x, int* la, char* filename);
 int indexABCol(int i, int j, int *lab);
 int dgbtrftridiag(int *la, int *n, int *kl, int *ku, double *AB, int *lab, int *ipiv, int *info);
 void LU_Facto(double* AB, int *lab, int *la, int *kv);      // Function to do LU factorisation
+
+
+void eig_poisson1D(double* eigval, int *la);
+double eigmax_poisson1D(int *la);
+double eigmin_poisson1D(int *la);
+double richardson_alpha_opt(int *la);
+void richardson_alpha(double *AB, double *RHS, double *X, double *alpha_rich, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite);
+void extract_MB_jacobi_tridiag(double *AB, double *MB, int *lab, int *la,int *ku, int*kl, int *kv);
+void extract_MB_gauss_seidel_tridiag(double *AB, double *MB, int *lab, int *la,int *ku, int*kl, int *kv);
+void richardson_MB(double *AB, double *RHS, double *X, double *MB, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite);
